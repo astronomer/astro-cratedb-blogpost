ebcc77a202d549bb388e0d5c6ef86849560fd5fd
Marija Selakovic <m.selakovic89@gmail.com>
2023-07-11 10:42:47 +0200

    DAG and query changes for new dataset

--- dags/etl_pipeline.py
+++ dags/etl_pipeline.py
@@ -1,29 +1,35 @@
+import os
+import pandas as pd
+
 from airflow.utils.dates import datetime
 from airflow.decorators import dag, task
 from airflow.models.baseoperator import chain
-
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from airflow.providers.common.sql.operators.sql import SQLColumnCheckOperator
 
-import seaborn as sns
-
-CREATE_DB_CONN_ID = "cratedb_connection"
+CRATE_DB_CONN_ID = "cratedb_connection"
+AWS_S3_BUCKET = os.getenv("AWS_S3_BUCKET")
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+key="possum/possum.csv"
 
 
 @task
 def extract_data():
-    # here we can use other datasets, or load from S3 if you have some interesting example
-    tips = sns.load_dataset("tips")
-    return tips
+    possum = pd.read_csv(   
+    f"s3://{AWS_S3_BUCKET}/{key}",
+    storage_options={
+        "key": AWS_ACCESS_KEY_ID,
+        "secret": AWS_SECRET_ACCESS_KEY
+    })
+    return possum
 
 
-# remove rows with empty column and add a new column total_paid
+# remove rows with empty column
 @task
 def transform_data(dataset):
-    tips_final = dataset.dropna()
-    tips_final["total_paid"] = tips_final["total_bill"] + tips_final["tip"]
-    print(tips_final.head(5).values.tolist())
-    return tips_final.values.tolist()
+    possum_final = dataset.dropna()
+    return possum_final.values.tolist()
 
 
 @dag(
@@ -35,8 +41,8 @@ def transform_data(dataset):
 )
 def etl_pipeline():
     create_table = SQLExecuteQueryOperator(
-        task_id="create_tips_table",
-        conn_id=CREATE_DB_CONN_ID,
+        task_id="create_possum_table",
+        conn_id=CRATE_DB_CONN_ID,
         sql="sql/create_table.sql",
     )
 
@@ -45,23 +51,23 @@ def etl_pipeline():
     transform = transform_data(extract)
 
     load_data = SQLExecuteQueryOperator.partial(
-        task_id="load_tips_data",
-        conn_id=CREATE_DB_CONN_ID,
+        task_id="load_possum_data",
+        conn_id=CRATE_DB_CONN_ID,
         sql="sql/insert_data.sql",
     ).expand(parameters=transform)
 
     select_data = SQLExecuteQueryOperator(
         task_id="select_data",
-        conn_id=CREATE_DB_CONN_ID,
-        sql="SELECT AVG(total_paid) FROM doc.tips",
+        conn_id=CRATE_DB_CONN_ID,
+        sql="SELECT AVG(age) FROM doc.possum",
     )
 
     data_check = SQLColumnCheckOperator(
         task_id="value_check",
-        conn_id=CREATE_DB_CONN_ID,
-        table="doc.tips",
+        conn_id=CRATE_DB_CONN_ID,
+        table="doc.possum",
         column_mapping={
-            "total_paid": {"min": {"geq_to": 0}},
+            "total_paid": {"age": {"geq_to": 0}},
         },
     )
 
