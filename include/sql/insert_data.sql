82e2352a98c28f16dbf5d55824777ed0b1725f39
marijaselakovic <m.selakovic89@gmail.com>
2023-07-13 09:17:14 +0200

    Update include/sql/insert_data.sql
    Co-authored-by: Tamara Janina Fingerlin <90063506+TJaniF@users.noreply.github.com>

--- include/sql/insert_data.sql
+++ include/sql/insert_data.sql
@@ -1,2 +1,2 @@
-INSERT INTO doc.possum (case, site, pop, sex, age, hdlngth, skullw, totlngth, taill, footlgth, earconch, eye, chest, belly)
+INSERT INTO doc.possum (case_num, site, pop, sex, age, hdlngth, skullw, totlngth, taill, footlgth, earconch, eye, chest, belly)
 VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
