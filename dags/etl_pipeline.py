500e935b9cb17921ec7cb4ab7f28db5b75af2fde
TJaniF <tamara.fingerlin@astronomer.io>
2023-07-20 11:47:06 +0200

    add final task and docstrings/readme

--- dags/etl_pipeline.py
+++ dags/etl_pipeline.py
@@ -1,11 +1,26 @@
-import os
-import pandas as pd
+"""
+## Run an ETL pipeline with CrateDB and data quality checks
 
-from airflow.utils.dates import datetime
+This DAG runs an ETL pipeline using the [CrateDB](https://crate.io/) database.
+The DAG will:
+- create a table in CrateDB
+- extract data from a CSV file in a public GH repository
+- transform the data in memory
+- load the data into CrateDB
+- run a data quality check on the data in CrateDB 
+- query the data in CrateDB
+- use XCom to print the query results as a log statement
+
+Define the connection to CrateDB as an environment variable in the format:
+AIRFLOW_CONN_CRATEDB_CONNECTION = "postgres://<your-user>:<your-password>@<your-host>:<your-port>/?sslmode=<your-ssl-mode>"
+"""
+
 from airflow.decorators import dag, task
 from airflow.models.baseoperator import chain
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from airflow.providers.common.sql.operators.sql import SQLColumnCheckOperator
+from airflow.utils.dates import datetime
+import pandas as pd
 
 CRATE_DB_CONN_ID = "cratedb_connection"
 
@@ -33,7 +48,7 @@ def transform_data(dataset):
 )
 def etl_pipeline():
     create_table = SQLExecuteQueryOperator(
-        task_id="create_possum_table",
+        task_id="create_table",
         conn_id=CRATE_DB_CONN_ID,
         sql="sql/create_table.sql",
     )
@@ -43,16 +58,10 @@ def etl_pipeline():
     transform = transform_data(extract)
 
     load_data = SQLExecuteQueryOperator.partial(
-        task_id="load_possum_data",
+        task_id="load_data",
         conn_id=CRATE_DB_CONN_ID,
         sql="sql/insert_data.sql",
     ).expand(parameters=transform)
-
-    select_data = SQLExecuteQueryOperator(
-        task_id="select_data",
-        conn_id=CRATE_DB_CONN_ID,
-        sql="sql/select.sql",
-    )
 
     data_check = SQLColumnCheckOperator(
         task_id="value_check",
@@ -63,5 +72,26 @@ def etl_pipeline():
         },
     )
 
+    select_data = SQLExecuteQueryOperator(
+        task_id="select_data",
+        conn_id=CRATE_DB_CONN_ID,
+        sql="sql/select.sql",
+    )
+
+    @task
+    def print_selected_data(**context):
+        selected_data = context["ti"].xcom_pull(task_ids="select_data")
+        print(selected_data)
+
+    chain(
+        create_table,
+        extract,
+        transform,
+        load_data,
+        data_check,
+        select_data,
+        print_selected_data(),
+    )
+
+
 etl_pipeline()
-    chain(create_table, extract, transform, load_data, data_check, select_data)
--- dags/test_cratedb.py
+++ dags/test_cratedb.py
@@ -1,20 +1,30 @@
+"""
+## DAG to test your CrateDB connection
+
+Define the connection to CrateDB as an environment variable in the format:
+AIRFLOW_CONN_CRATEDB_CONNECTION = "postgres://<your-user>:<your-password>@<your-host>:<your-port>/?sslmode=<your-ssl-mode>"
+"""
+
 from airflow.decorators import dag
 from airflow.providers.postgres.operators.postgres import PostgresOperator
-from pendulum import datetime 
+from pendulum import datetime
+
+
+CRATE_DB_CONN_ID = "cratedb_connection"
 
 
 @dag(
-    start_date=datetime(2021, 1, 1),
+    start_date=datetime(2023, 7, 1),
     schedule=None,
     catchup=False,
 )
 def test_cratedb():
-
-    f = PostgresOperator(
-        task_id="test",
+    PostgresOperator(
+        task_id="test_connection",
         database="dev",
         sql="SELECT name FROM sys.cluster",
-        postgres_conn_id="cratedb_connection",
+        postgres_conn_id=CRATE_DB_CONN_ID,
     )
+
 
 test_cratedb()
