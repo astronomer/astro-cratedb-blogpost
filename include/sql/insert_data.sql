db427397919b2882237f53f9a3de7f161ad8c3a0
TJaniF <tamara.fingerlin@astronomer.io>
2023-07-24 12:02:46 +0200

    the sql that does not ddos the database

--- include/sql/insert_data.sql
+++ include/sql/insert_data.sql
@@ -1,5 +1,3 @@
 INSERT INTO doc.possum (case_num, site, pop, sex, age, hdlngth, skullw, totlngth, taill, footlgth, earconch, eye, chest, belly)
 SELECT a, b, c, d, e, f, g, h, i, j, k, l, m, n
-FROM UNNEST(%s) a, UNNEST(%s) b, UNNEST(%s) c, UNNEST(%s) d, UNNEST(%s) e, 
-     UNNEST(%s) f, UNNEST(%s) g, UNNEST(%s) h, UNNEST(%s) i, UNNEST(%s) j, 
-     UNNEST(%s) k, UNNEST(%s) l, UNNEST(%s) m, UNNEST(%s) n;
+FROM UNNEST(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) AS t(a, b, c, d, e, f, g, h, i, j, k, l, m, n);
